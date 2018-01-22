aa3b0e1452bd44b681112fb0c9e8a5bc17d8d1c0
zizhongtank <32141317+zizhongtank@users.noreply.github.com>
2018-01-22 14:22:10 -0500

    unity simple path finding
    using navigation system

---
+++ Scripts/Finder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finder : MonoBehaviour {
+
+    public Transform destinationPoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = destinationPoint.position;
+	}
+}
